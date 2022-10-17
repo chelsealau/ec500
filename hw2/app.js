d19ca25a3efb3094e8706af6c75c85ece87cc3e1
JaeYoonChung <jcdino@bu.edu>
2022-10-17 17:35:02 -0400

    chage color format

--- hw2/app.js
+++ hw2/app.js
@@ -133,13 +133,13 @@ function md5(salt) {
 }
 
 function updateSubTotal() {
-    document.getElementById('sumScore').style.backgroundColor = "white";
+    document.getElementById('sumScore').style.backgroundColor = "#ffffff00";
     var arr = document.getElementsByName('score');
     var tot=0;
     for(var i=0;i<arr.length;i++){
         if(parseInt(arr[i].value))
             tot += parseInt(arr[i].value);
-            arr[i].style.backgroundColor = "white";
+            arr[i].style.backgroundColor = "#ffffff";
             if (parseInt(arr[i].value) < 0 || parseInt(arr[i].value) > parseInt(document.getElementById('maxRank').value)){
                 alert(arr[i].id + " RANK IS OUT OF RANGE(0-"+document.getElementById('maxRank').value+")");
                 arr[i].style.backgroundColor = "#d9361a";
