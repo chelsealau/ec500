a8984a5872847c204d352fe869a49bc354ff03cb
JaeYoonChung <jcdino@bu.edu>
2022-10-17 08:24:52 -0400

    edit alert to notify which rank is wrong

--- hw2/app.js
+++ hw2/app.js
@@ -9,13 +9,17 @@ function updateSubTotal() {
             tot += parseInt(arr[i].value);
             arr[i].style.backgroundColor = "white";
             if (parseInt(arr[i].value) < 0 || parseInt(arr[i].value) > 100){
-                alert("RANK IS OUT OF RANGE (0-100)");
+                alert(arr[i].id + " RANK IS OUT OF RANGE (0-100)");
                 arr[i].style.backgroundColor = "#d9361a";
             }
         }
     document.getElementById('sumScore').innerHTML = tot;
     if (tot>100){
         document.getElementById('sumScore').style.backgroundColor = "#d9361a";
-        alert("TOTAL RANK IS OVER 100")
+        alert("SUM OF RANK IS OVER 100")
+    }
+    if (tot<0){
+        document.getElementById('sumScore').style.backgroundColor = "#d9361a";
+        alert("SUM OF RANK IS UNDER 0")
     }
 }
