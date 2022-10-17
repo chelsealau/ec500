60827aff4ff0939f42397b9367619e95a9bd1b5d
JaeYoonChung <jcdino@bu.edu>
2022-10-17 00:36:18 -0400

    check if sum over 100

--- hw2/app.js
+++ hw2/app.js
@@ -1,12 +1,16 @@
 'use strict';
 
 function updateSubTotal() {
+    document.getElementById('sumScore').style.backgroundColor = "white";
     var arr = document.getElementsByName('score');
     var tot=0;
     for(var i=0;i<arr.length;i++){
         if(parseInt(arr[i].value))
             tot += parseInt(arr[i].value);
-    console.log(tot)
-    }
+            arr[i].style.backgroundColor = "white";
+        }
     document.getElementById('sumScore').innerHTML = tot;
+    if (tot>100){
+        document.getElementById('sumScore').style.backgroundColor = "#d9361a";
+    }
 }
